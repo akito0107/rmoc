fa886363c7a529d5ba03fd3a2f410ba364e1f778
Akito Ito <akito0107@gmail.com>
2018-09-17 00:07:21 +0900

    add tests

--- rmoc_test.go
+++ rmoc_test.go
@@ -2,6 +2,8 @@ package rmoc
 
 import (
 	"bytes"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -12,4 +14,35 @@ func TestCreateFileWithAbort(t *testing.T) {
 	if ok, _ := IsFileAlreadyExists(err); !ok {
 		t.Errorf("error must be FileAlreadyExists byt %+v", err)
 	}
+}
+
+func TestOverrideFile(t *testing.T) {
+	func() {
+		pre := bytes.NewBufferString("pre")
+		out, err := os.Create("testdata/override.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer out.Close()
+		io.Copy(out, pre)
+	}()
+
+	after := bytes.NewBufferString("after")
+	if err := OverrideFile(after, "testdata", "override.txt"); err != nil {
+		t.Fatal(err)
+	}
+
+	f, err := os.Open("testdata/override.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	var dist bytes.Buffer
+	io.Copy(&dist, f)
+
+	if dist.String() != "after" {
+		t.Errorf("File contents must be after but %s", dist.String())
+	}
+
 }
